034056a00e3f9cb05294c886b703d7ff0e76e627
Yu Duo Zhang <2480549@edu.vaniercollege.qc.ca>
2025-10-15 10:47:35 -0400

    Initialize Task 2

---
+++ src/vaniercollege/zyd/ZYD_HouseWithWindowPanesLab.java
@@ -0,0 +1,21 @@
+package vaniercollege.zyd;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author Yu Duo Zhang(2480549)
+ */
+public class ZYD_HouseWithWindowPanesLab extends Application {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage stage) {
+        
+    }
+}

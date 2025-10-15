2c9b749f37d80c7c689275d80f9a02ec3da1a402
Yu Duo Zhang <2480549@edu.vaniercollege.qc.ca>
2025-10-15 10:28:16 -0400

    Add Constants Defined in Instructions

--- src/vaniercollege/zyd/YuDuoZhang_Lab06.java
+++ src/vaniercollege/zyd/YuDuoZhang_Lab06.java
@@ -4,10 +4,12 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.*;
 import javafx.scene.shape.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
  * @author Yu Duo Zhang (2480549)
+ * https://github.com/zyd-074/Vanier_F25_420-SF3-RE_Lab06.git
  */
 public class YuDuoZhang_Lab06 extends Application {
 
@@ -18,18 +20,27 @@ public class YuDuoZhang_Lab06 extends Application {
         launch(args);
     }
 
+    /**
+     * Task 1
+     * @param stage stage of the program
+     */
     @Override
     public void start(Stage stage) {
-        Rectangle rect = new Rectangle();
-        rect.setWidth(10);
-        rect.setHeight(15);
-        rect.setX(100);
-        rect.setY(200);
+        // Constants for the scene size
+        final double SCENE_WIDTH = 520.0;
+        final double SCENE_HEIGHT = 520.0;
         
-        HBox root = new HBox();
+        // Constants for each square's XY coorfinates
+        final int X1 = 10, Y1 = 10;
+        final int X2 = 60, Y2 = 60;
+        final int X3 = 110, Y3 = 110;
         
-        Scene scene = new Scene(root, 300, 200);
-        stage.setScene(scene);
-        stage.show();
+        // Constants for each square's width and height
+        final int WIDTH1 = 500, HEIGHT1 = 500;
+        final int WIDTH2 = 400, HEIGHT2 = 400;
+        final int WIDTH3 = 300, HEIGHT3 = 300;
+        
+        // Constants for the circle's geometry
+        final int CENTER_X = 260, CENTER_Y = 260, RADIUS = 150;
     }
 }

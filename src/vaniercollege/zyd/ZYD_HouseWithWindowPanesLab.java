0d82b224f3adff2868f2bb58522730e60da0f2a7
Yu Duo Zhang <2480549@edu.vaniercollege.qc.ca>
2025-10-15 11:08:12 -0400

    Add Chemney & Door

--- src/vaniercollege/zyd/ZYD_HouseWithWindowPanesLab.java
+++ src/vaniercollege/zyd/ZYD_HouseWithWindowPanesLab.java
@@ -27,12 +27,23 @@ public class ZYD_HouseWithWindowPanesLab extends Application {
         
         // Roof
         Polygon roof = new Polygon(100,230,400,230,250,100);
-        roof.setFill(Color.BROWN);
+        roof.setFill(Color.CHOCOLATE);
         roof.setStroke(Color.BLACK);
+        
+        // Chemney
+        Rectangle chemney = new Rectangle(175, 120, 25, 75);
+        chemney.setFill(Color.GRAY);
+        chemney.setStroke(Color.BLACK);
+        
+        // Door
+        Rectangle door = new Rectangle(275, 325, 60, 105);
+        Circle doorHandle = new Circle(325, 390, 5);
+        door.setFill(Color.MAROON);
+        doorHandle.setFill(Color.WHITE);
         
         // Display
         Pane root = new Pane();
-        root.getChildren().addAll(base,roof);
+        root.getChildren().addAll(base,roof, chemney, door, doorHandle);
         Scene scene = new Scene(root, 500, 500);
         stage.setScene(scene);
         stage.show();

a16cc4a79fe874aaa84292a3a42c32e6491e0035
Yu Duo Zhang <2480549@edu.vaniercollege.qc.ca>
2025-10-15 10:57:42 -0400

    Add Base & Roof & Display

--- src/vaniercollege/zyd/ZYD_HouseWithWindowPanesLab.java
+++ src/vaniercollege/zyd/ZYD_HouseWithWindowPanesLab.java
@@ -1,6 +1,10 @@
 package vaniercollege.zyd;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
 import javafx.stage.Stage;
 
 /**
@@ -16,6 +20,21 @@ public class ZYD_HouseWithWindowPanesLab extends Application {
     
     @Override
     public void start(Stage stage) {
+        // Base
+        Rectangle base = new Rectangle(100, 230, 300, 200);
+        base.setFill(Color.LIGHTGRAY);
+        base.setStroke(Color.BLACK);
         
+        // Roof
+        Polygon roof = new Polygon(100,230,400,230,250,100);
+        roof.setFill(Color.BROWN);
+        roof.setStroke(Color.BLACK);
+        
+        // Display
+        Pane root = new Pane();
+        root.getChildren().addAll(base,roof);
+        Scene scene = new Scene(root, 500, 500);
+        stage.setScene(scene);
+        stage.show();
     }
 }

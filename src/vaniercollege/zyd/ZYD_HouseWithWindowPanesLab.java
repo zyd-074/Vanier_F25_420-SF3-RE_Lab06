94b0a5c6725a1b3c360d1c6258dc55afa18a8304
Yu Duo Zhang <2480549@edu.vaniercollege.qc.ca>
2025-10-15 11:18:13 -0400

    Add Windows & Moved Door

--- src/vaniercollege/zyd/ZYD_HouseWithWindowPanesLab.java
+++ src/vaniercollege/zyd/ZYD_HouseWithWindowPanesLab.java
@@ -36,14 +36,28 @@ public class ZYD_HouseWithWindowPanesLab extends Application {
         chemney.setStroke(Color.BLACK);
         
         // Door
-        Rectangle door = new Rectangle(275, 325, 60, 105);
-        Circle doorHandle = new Circle(325, 390, 5);
+        Rectangle door = new Rectangle(230, 325, 60, 105);
+        Circle doorHandle = new Circle(280, 390, 5);
         door.setFill(Color.MAROON);
         doorHandle.setFill(Color.WHITE);
         
+        // Windows
+        Rectangle windowLeft = new Rectangle(130, 280, 70, 70);
+        Rectangle windowRight = new Rectangle(310, 280, 70, 70);
+        windowLeft.setFill(Color.LIGHTBLUE);
+        windowRight.setFill(Color.LIGHTBLUE);
+        Line vertLeft = new Line(165,280,165,350);
+        Line vertRight = new Line(345,280,345,350);
+        Line horLeft = new Line(130, 315, 200, 315);
+        Line horRight = new Line(310, 315, 380, 315);
+        
         // Display
         Pane root = new Pane();
-        root.getChildren().addAll(base,roof, chemney, door, doorHandle);
+        root.getChildren().addAll(base,roof,chemney,
+                door, doorHandle,
+                windowLeft, windowRight,
+                vertLeft, vertRight,
+                horLeft, horRight);
         Scene scene = new Scene(root, 500, 500);
         stage.setScene(scene);
         stage.show();
